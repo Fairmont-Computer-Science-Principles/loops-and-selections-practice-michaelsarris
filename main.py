72b92c93704b0009d0f5dba6d0e47309ed50d90a
Fairmont Prep Computer Science Courses <diramirez@fairmontschools.com>
2023-09-08 14:31:24 -0700

    completed assignment

--- main.py
+++ main.py
@@ -14,14 +14,14 @@
 #--------write your code below ----- #
 
 
-Step 1: declare num  0, stars  “**”
-Step 2: create a loop to repeat total_lines_times
-Step 3: create and if else statement to be able to
-‘select’ the top half and bottom half of the print
-statements. Write conditional statement using i
-Step 4 Increase num if i is less than half_total_lines
-Step 5. use num to multiple times stars
-DISPLAY (stars x num)
-Step 6. else decrease num by 1
-Step 7. DISPLAY (stars x num)
+num = 1
+stars = "**"
 
+for i in range(10):
+    if(i<4):
+        
+        print(stars*num)
+        num+=1
+    else: 
+        print(stars*num)
+        num-=1
